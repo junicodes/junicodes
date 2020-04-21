aab4603fbf58e326e7b80f42f6452277fb8a1b75
junicodefire <junicodefire@gmail.com>
2020-04-21 17:44:52 +0100

    Updates

---
+++ precache-manifest.6beb818fa0575fc0f8b3a0f3b2ee980f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f1feb6a4e2dae1297f979aa2db34bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b04dc348e38e832183f",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "ecf63ac34751d2e91338",
+    "url": "/static/css/main.98a562fb.chunk.css"
+  },
+  {
+    "revision": "6b04dc348e38e832183f",
+    "url": "/static/js/2.361e057f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.361e057f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecf63ac34751d2e91338",
+    "url": "/static/js/main.74892e34.chunk.js"
+  },
+  {
+    "revision": "775ad2858b7356590793",
+    "url": "/static/js/runtime-main.c6c254df.js"
+  }
+]);
