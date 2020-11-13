3023d1df26298347669c7da006863d5a5451c3ef
junicodefire <junicodefire@gmail.com>
2020-11-13 10:48:21 +0100

    fix

--- src/components/paths/Portfolio/Portfolio.js
+++ src/components/paths/Portfolio/Portfolio.js
@@ -18,7 +18,7 @@ const Portfolio = () => {
                                 }}>
                                 <a href="https://developnd.ng" target="_blank" rel="noopener noreferrer">
                                     <h5>DevelopND</h5>
-                                    <p><small>Tech Used: Laravel, HTML, SASS, JavaScript</small></p>
+                                    <p><small>Tech Used: Laravel, HTML, SASS, MySQL, Digital Ocean (NGINX), JavaScript</small></p>
                                     <p>This is a web app that tracks ongoing projects awarded to communities in Niger Delta Region of 
                                         Nigeria and also create the oppurturnity for community memeber to interact with project owners.
                                     </p>
@@ -31,10 +31,10 @@ const Portfolio = () => {
                                 }}>
                                 <a href="https://gateguard.co" target="_blank" rel="noopener noreferrer">
                                     <h5>GateGuard</h5>
-                                    <p><small>Tech Used: Laravel, Flutter, HTML, CSS, JavaScript</small></p>
+                                    <p><small>Tech Used: Laravel, Flutter, HTML, CSS, JavaScript, Vue.js</small></p>
                                     <p>This is a security estate management app, it was build as a mobile app
-                                        that allows user to control thier visitors 
-                                        and also help estates managers to manage there estate bills.
+                                        that allows user to control thier visitors, pay bills, order for domestic services
+                                        and also help estates managers to manage there estate data and bills.
                                     </p>
                                 </a>
                             </div>
@@ -43,21 +43,10 @@ const Portfolio = () => {
                             <div className="build-show animated fadeIn" style={{animationDelay: '0.6s', 
                                 animationTimingFunction: 'linear'
                                 }}>
-                                <a href="https://permollys.com/" target="_blank" rel="noopener noreferrer">
-                                    <h5>Permollys</h5>
-                                    <p><small>Tech Used: Wordpress, Php</small></p>
-                                    <p>A lifestyle blog website owned and managed by Precious Okechukwu.</p>
-                                </a>
-                            </div>
-                        </div>
-                        <div className="p-3">
-                            <div className="build-show animated fadeIn" style={{animationDelay: '0.6s', 
-                                animationTimingFunction: 'linear'
-                                }}>
-                                <a href="https://adsvilla.org/" target="_blank" rel="noopener noreferrer">
-                                    <h5>AdsVilla</h5>
-                                    <p><small>Tech Used: NodeJS, HTML, CSS, JS,</small></p>
-                                    <p>An Advertising agency based in the city of Abuja, Nigeria.</p>
+                                <a href="https://trim.ng/" target="_blank" rel="noopener noreferrer">
+                                    <h5>Trim.NG</h5>
+                                    <p><small>Tech Used: Node.js, JavaScript</small></p>
+                                    <p>A well designed url shortener.</p>
                                 </a>
                             </div>
                         </div>
@@ -67,7 +56,7 @@ const Portfolio = () => {
                                 }}>
                                 <a href="https://pfxnet.com/" target="_blank" rel="noopener noreferrer">
                                     <h5>Pfxnet</h5>
-                                    <p><small>Tech Used: NodeJS, HTML, CSS, JS,</small></p>
+                                    <p><small>Tech Used: NodeJS (Express.js), MongoDB, HTML, CSS, JS,</small></p>
                                     <p>An Estate Managemnet and investment platform.</p>
                                 </a>
                             </div>
@@ -76,8 +65,19 @@ const Portfolio = () => {
                             <div className="build-show animated fadeIn" style={{animationDelay: '0.6s', 
                                 animationTimingFunction: 'linear'
                                 }}>
+                                <a href="https://adsvilla.org/" target="_blank" rel="noopener noreferrer">
+                                    <h5>CherrySoft Deliveries (Ongoing Build)</h5>
+                                    <p><small>Tech Used: NodeJS (Adonis.js), React.js, MySQL</small></p>
+                                    <p>A day to day delivery services located at Port Harcourt, Rivers State.</p>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="p-3">
+                            <div className="build-show animated fadeIn" style={{animationDelay: '0.6s', 
+                                animationTimingFunction: 'linear'
+                                }}>
                                 <a href="https://ewoma.herokuapp.com" target="_blank" rel="noopener noreferrer">
-                                    <h5>Ewoma Bot</h5>
+                                    <h5>Ewoma Bot (Personal)</h5>
                                     <p><small>Tech Used: Node.js, Socket.io, JavaScript, Dialog Flow</small></p>
                                     <p>An AI Bot that give information about covid-19 virus from all reliable sources,
                                         it also shares web links to get more information.</p>
@@ -89,24 +89,13 @@ const Portfolio = () => {
                                 animationTimingFunction: 'linear'
                                 }}>
                                 <a href="https://reactmoviebox.herokuapp.com/" target="_blank" rel="noopener noreferrer">
-                                    <h5>Movie Box</h5>
-                                    <p><small>Tech Used: Node.js, Socket.io, JavaScript, Dialog Flow</small></p>
-                                    <p>An AI Bot that give information about covid-19 virus from all reliable sources,
-                                        it also shares web links to get more information.</p>
+                                    <h5>Movie Box (Personal)</h5>
+                                    <p><small>Tech Used: Node.js, Socket.io, React.js, Dialog Flow</small></p>
+                                    <p>Here I am leveraging the use of MovieDB API.</p>
                                 </a>
                             </div>
                         </div>
-                        <div className="p-3">
-                            <div className="build-show animated fadeIn" style={{animationDelay: '0.8s', 
-                                animationTimingFunction: 'linear'
-                                }}>
-                                <a href="https://primedpoll.com" target="_blank" rel="noopener noreferrer">
-                                    <h5>Primedpoll</h5>
-                                    <p><small>Tech Used: Laravel, HTML, CSS, JavaScript, </small></p>
-                                    <p>This is a polling site that allows a user to create and vote a poll.</p>
-                                </a>
-                            </div>
-                        </div>
+
                     </div>
                 </div>
             </div>
